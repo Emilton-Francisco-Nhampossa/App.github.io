591b80d9012b01d9913224e55d28105426017412
Emilton Francisco <emiltonnhampossa0@gmail.com>
2023-01-30 19:36:18 +0200

    scroll-commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="q-pa-lg">
+   <h5 class="q-mt-none">Help</h5>
+   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex aliquid in temporibus assumenda officiis mollitia ipsum tempore doloribus incidunt saepe et, nesciunt quo ab repellat ratione ea itaque consequuntur totam. </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ipsam non quas deleniti, tempore accusantium, soluta voluptates voluptate dolorem voluptas voluptatum obcaecati commodi est ullam sit ducimus quidem expedita mollitia?</p>
+   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione rem nemo doloremque qui magni officiis deleniti dolor. Beatae aliquam quae ut, repellendus voluptas vel, molestiae mollitia neque harum enim tempore?</p>
+  </q-page>
+</template>
+
+<script>
+export default{
+  data(){
+
+  }
+}
+</script>
